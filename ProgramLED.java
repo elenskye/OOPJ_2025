22f63132fecf9c0461562b696de1a6fb8e828cd7
Elen Skye <143269262+elenskye@users.noreply.github.com>
2025-07-29 11:40:24 +0800

    Create ProgramLED.java

---
+++ ProgramLED.java
@@ -0,0 +1,10 @@
+public class ProgramLED {
+	public static void main(String[] args) {
+		LED_Lamp lamp = new LED_Lamp();
+		lamp.showLight(0);
+		lamp.showLight(1);
+		lamp.showLight(2);
+		lamp.showLight(3);
+		lamp.showLight(0);
+	}
+}
